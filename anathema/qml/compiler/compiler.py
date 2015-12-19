dae474ea116d282bb427f529f042931af22a2438
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-12-20 00:42:37 +0300

    added semantics bootstrap

--- anathema/qml/compiler/compiler.py
+++ anathema/qml/compiler/compiler.py
@@ -1,10 +1,17 @@
 import grammar
 
+class IRFactory(grammar.qmlSemantics):
+	def property_declaration(self, ast):
+		print "property declaration", ast
+		return ast
+
+
 class Compiler(object):
 	def __init__(self):
 		pass
 
-	def compile(self, data, fname):
+	def compile(self, data, filename = None, trace = False):
 		parser = grammar.qmlParser()
-		ast = parser.parse(data, 'component_declaration', filename = fname, trace = True)
+		ir = IRFactory()
+		ast = parser.parse(data, 'component_declaration', filename = filename, trace = trace, semantics = ir)
 		print ast
